81dd53f881e6e04e61cee58530bfe67db5274199
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-14 15:08:58 +0100

    Initial Commit

---
+++ gui app/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  gui app
+//
+//  Created by Жека Г on 2/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
